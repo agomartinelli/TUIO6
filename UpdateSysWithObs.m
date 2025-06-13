20e3bff30a583d8d8d51a1160c8152b9058d2aa0
agomartinelli <agostino.martinelli@inria.fr>
2025-06-13 08:30:00 +0200

    Update UpdateSysWithObs.m

--- UpdateSysWithObs.m
+++ UpdateSysWithObs.m
@@ -1,6 +1,6 @@
 function sys = UpdateSysWithObs(sys, OBS)
     % Updates the system structure with the observability codistribution
-    % and th enew observable functions
+    % and the new observable functions
     
     sys.hobs = OBS.h;
     sys.O = OBS.O;
