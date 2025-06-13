3e0b39bf10c3c64a1a87437b86915bdc88575f5c
agomartinelli <agostino.martinelli@inria.fr>
2025-06-13 08:51:40 +0200

    Update set_tool.m

--- set_tool.m
+++ set_tool.m
@@ -2,9 +2,9 @@ function sys = set_tool(sys)
     % Set tool parameters and initialize necessary variables
 
     % Initialize the state estimate vector 'xe' with random values
-    % The vector size is (n + 10 * m_w), where 'n' is the number of states and 'm_w' is the number of unknown inputs
+    % The vector size is (n+1+n*m_w^2), where 'n' is the number of states and 'm_w' is the number of unknown inputs
     % 'xe' will later be used for rank calculations in the subsequent steps
-    sys.tool.xe = rand(sys.n + 1 + 10 * sys.m_w, 1);
+    sys.tool.xe = rand(sys.n*(1 + sys.m_w^2)+1, 1);
 
     % Set the tolerance value for the tool (used in various computations)
     sys.tool.cTOL = 1e5;
